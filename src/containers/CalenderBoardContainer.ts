2a51c79f0bfbedf2009076fb1447c1a817e03ec2
kumashun8 <tiwo3wa4@gmail.com>
2020-03-04 22:30:15 +0900

    Create dispatch for CalenderBoard

--- src/containers/CalenderBoardContainer.ts
+++ src/containers/CalenderBoardContainer.ts
@@ -1,6 +1,6 @@
 import { Appstate } from 'redux/store';
 import { connect } from 'react-redux';
-import { InputActions } from 'redux/actions';
+import { CalenderActions } from 'redux/actions';
 import { CalenderBoard } from 'components/CalenderBoard';
 import { Dispatch } from 'react';
 
@@ -9,10 +9,8 @@ interface StateAtProps {
   month: number;
 }
 
-export type TopPageHandler = {
-  handleOnChangeValue(value: string): void;
-  handleOnSelectValue(value: string): void;
-  handleOnClick(): void;
+export type CalenderBoardHandler = {
+  handleChangeMonth(value: { year: number; month: number }): void;
 };
 
 const mapStateToProps = (appState: Appstate): StateAtProps => {
@@ -22,16 +20,10 @@ const mapStateToProps = (appState: Appstate): StateAtProps => {
   };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch<any>): TopPageHandler => {
+const mapDispatchToProps = (dispatch: Dispatch<any>): CalenderBoardHandler => {
   return {
-    handleOnChangeValue: (value: string) => {
-      dispatch(InputActions.updateTextInputValue(value));
-    },
-    handleOnSelectValue: (value: string) => {
-      dispatch(InputActions.updateSelectedValue(value));
-    },
-    handleOnClick: () => {
-      dispatch(InputActions.updateCount());
+    handleChangeMonth: ({ year, month }) => {
+      dispatch(CalenderActions.updateCurrentMonth({ year, month }));
     },
   };
 };
