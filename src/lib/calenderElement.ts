fc1b6dd22a93166b51e0a3cdaf8d52140a12d2e1
kumashun8 <tiwo3wa4@gmail.com>
2020-03-07 16:15:11 +0900

    Revert "Merge pull request #7 from kumashun8/revert-6-feature/remove-event"
    This reverts commit 04f39fd3985821a269d43abe7ceb25158125b817, reversing
    changes made to fe7313db9dcb49645d2b64ed3a7df78a9cfe5769.

--- src/lib/calenderElement.ts
+++ src/lib/calenderElement.ts
@@ -6,17 +6,24 @@ interface CalenderElementable {
   readonly isInCurrentMonth: boolean;
   readonly events: string[];
   readonly omittedEvents: string[];
+  readonly eventIds: number[];
 }
 
 export class CalenderElement implements CalenderElementable {
   private _events: string[] = [];
   private _omittedEvents: string[] = [];
+  private _eventIds: number[] = [];
   constructor(readonly date: number, readonly isInCurrentMonth: boolean) {}
   filterEvents(innerEvents: Array<InnerEvent>): void {
-    this.events = innerEvents
+    const tmpEvents: [string, number][] = innerEvents
       .filter(n => n.date === this.date)
-      .map(m => m.value);
-    this.omittedEvents = this.events.map(n => omitt(n));
+      .map(m => [m.value, m.baseId]);
+    for (let index = 0; index < tmpEvents.length; index++) {
+      const element = tmpEvents[index];
+      this.events.push(element[0]);
+      this.eventIds.push(element[1]);
+      this.omittedEvents.push(omitt(element[0]));
+    }
   }
   get events(): string[] {
     return this._events;
@@ -29,5 +36,11 @@ export class CalenderElement implements CalenderElementable {
   }
   set omittedEvents(newEvents: string[]) {
     this._omittedEvents = newEvents;
+  }
+  get eventIds(): number[] {
+    return this._eventIds;
+  }
+  set eventIds(newIds: number[]) {
+    this._eventIds = newIds;
   }
 }
